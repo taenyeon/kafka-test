8c2994ce02a49240dcbe3c7e15beded77ac5dc80
taenyeon <tae8753@gmail.com>
2022-04-13 22:30:33 +0900

    kafka batch test (fail)

--- src/main/java/com/example/springtest/controller/TestController.java
+++ src/main/java/com/example/springtest/controller/TestController.java
@@ -1,18 +1,25 @@
 package com.example.springtest.controller;
 
-import com.example.springtest.service.TestProducerService;
+import com.example.springtest.service.TestService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
-    private final TestProducerService testProducerService;
+    private final TestService testService;
 
-    public TestController(TestProducerService testProducerService) {
-        this.testProducerService = testProducerService;
+    public TestController(TestService testService) {
+        this.testService = testService;
     }
+
     @PostMapping(value = "/sendMessage")
-    public void sendMessage(String message){
-        testProducerService.sendMessage(message);
+    public void sendMessage(String message) throws InterruptedException {
+        int i = 0;
+        while (i<1000){
+            testService.sendMessage(message);
+            Thread.sleep(10);
+            i++;
+        }
     }
+
 }
